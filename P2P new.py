3879c2e910152f66ce2fa1fe0b2c2cdba3e12a17
hdsef <31828672+hdsef@users.noreply.github.com>
2018-03-06 00:39:40 +0300

    Update P2P new.py

--- P2P new.py
+++ P2P new.py
@@ -1,48 +1,76 @@
 import socket
 import threading
-from tkinter import *
 
 users = []
+MESSAGE = ""
+event = threading.Event()
+event.set()
 
-def connect_button(MESSAGE):
+def notification():
     UDP_PORT = 5005
-    print ("UDP target port:", UDP_PORT)
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
+    print("UDP target port:", UDP_PORT)
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # UDP
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
     sock.sendto(MESSAGE.encode(), ('<broadcast>', UDP_PORT))
-    
-def Receiving(MESSAGE):
+
+
+def Receiving():
     UDP_PORT = 5005
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # UDP
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # UDP
     sock.bind(('', UDP_PORT))
     while True:
-        data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
-        if data.decode().find("@#"):
-            f=0
+        event.wait()
+        print("1")
+        data, addr = sock.recvfrom(1024)
+        if data.decode().find("@#") != -1:
+            writeback = data.decode().split("@#")
+            flag = 0
             for item in users:
                 if item == data.decode():
-                    f=1
-            if f!=1:       
+                    flag = 1
+            if flag != 1:
                 users.append(data.decode())
-                writeback=data.decode().split("@#")
-                print(writeback[0])
-                sock.sendto(MESSAGE.encode(),(writeback[0],UDP_PORT))
-        else:
-            print ("received message:", data.decode())
-        
-#def main():
-message=input()
-MESSAGE = socket.gethostbyname(socket.getfqdn())+"@#"+message
-c = threading.Thread(target=Receiving(MESSAGE))
-c.daemon = True
-c.start()
+                print(writeback[0], "gdohndoh")
+                # sock.bind((writeback[0],UDP_PORT))
+                if data.decode() != MESSAGE:
+                    sock.sendto(MESSAGE.encode(), (writeback[0], UDP_PORT))
+        elif data.decode().find("|") != -1:
+            writeback = data.decode().split("|")
+            print("received message:", data.decode())
+            chat = open("chat"+writeback[0]+".txt", "a")
+            chat.write(writeback[1])
+            chat.close()
+
+
+def Sending(Actual_chat):
+    Node = input()
+    UDP_PORT = 5005
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # UDP
+    i=0
+    while i< len(users):
+        IP = users[i].split("@#")
+        if open_chat == IP[1]:
+            event.clear()
+            sock.sendto(message+"|"+Node.encode(), (IP[0], UDP_PORT))
+            event.set()
+        i += 1
+
+# def main():
 print("Please choose login")
+message = input()
+MESSAGE = socket.gethostbyname(socket.getfqdn())+"@#"+message
+users.append(socket.gethostbyname(socket.getfqdn())+"@#"+message)
+c = threading.Thread(target=Receiving)
+c.start()
+notification()
 
-#t = threading.Thread(target=connect_button(message))
-#t.daemon = True
-#t.start()
-connect_button(MESSAGE)
+print("open chat with")
+open_chat = input()
+file = open("chat"+open_chat+".txt", "r")
+for line in file:
+    print(line, end="")
+file.close()
+Sending(open_chat)
 
-messagf = input()
 for item in users:
     print(item)
